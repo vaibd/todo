cd62537854639c489faf7c7d82e77d197859a5d9
Vaibhav Dalakoti <38762713+vaibd@users.noreply.github.com>
2022-02-15 05:53:44 +0000

    add comments

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ let taskArray = ["Learn React", "Learn new ES6 syntax", "Complete the project"];
 let selectElement = [];
 displayTask(); //calling showTask function
 
-let checkBoxState = document.querySelector(".abcd").checked;
-console.log(checkBoxState);
 addButton.addEventListener("click", () => {
   addTask(displayTask);
 });
@@ -24,34 +22,32 @@ function addTask(displayTaskCallback) {
   if (taskName == "") {
     window.alert("Please enter something");
   } else {
-    let taskNameStorage = localStorage.getItem("New Todo"); //getting localstorage
+    let taskNameStorage = sessionStorage.getItem("todoStorage"); //getting sessionStorage
 
     taskArray.push(taskName); //pushing or adding new value in array
-    localStorage.setItem("New Todo", taskArray); //transforming js object into a json string
+    sessionStorage.setItem("todoStorage", taskArray); //transforming js object into a json string
     displayTaskCallback(); //calling showTask function
   }
 }
 
 // display the tasks in the local storage
 function displayTask() {
-  let taskNameStorage = localStorage.getItem("New Todo");
+  let taskNameStorage = sessionStorage.getItem("todoStorage");
 
   let newListTag = "";
   taskArray.forEach((element, index) => {
     newListTag += `
     
     <div class="listElement">
-
        <div> 
-       <input class = "abcd" type="checkbox" onclick = "multiSelect(${index})">
-        </div>
-         <div class="displayElement"> &nbsp ${element} &nbsp </div> 
-        <div class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash del"></i></div>
-
+          <input type="checkbox" onclick = "multiSelect(${index})">
+       </div>
+       <div class="displayElement"> &nbsp ${element} &nbsp </div> 
+       <div class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash del"></i></div>
     </div>
     `;
   });
-  list.innerHTML = newListTag; //adding new list tag inside ul tag
+  list.innerHTML = newListTag; //adding new list tag
   inputTask.value = ""; //once task added leave the input field blank
 }
 
@@ -60,37 +56,31 @@ function multiSelect(index) {
   selectElement.push(index);
   // console.log(selectElement);
   // console.log("before" + taskArray);
-  console.log(checkBoxState);
 }
 
 //delete selected elements
 function multiDelete() {
-  selectElement.map((elementIndex) => {
+  selectElement.forEach((elementIndex) => {
     if (elementIndex > -1) {
       taskArray.splice(elementIndex, 1);
     }
 
-    console.log("after " + taskArray);
+    // console.log("after " + taskArray);
   });
-
-  // for (i = 0; i <= selectElement.length; i++) {
-  //   taskArray.splice(selectElement[i], 1);
-  // }
   displayTask();
-  selectElement = [];
+  selectElement = []; //empty the array
 }
 
 // delete particular task
 function deleteTask(index) {
-  //taskArray = JSON.parse(taskNameStorage);
   taskArray.splice(index, 1); //delete or remove the li
-  localStorage.setItem("New Todo", taskArray);
+  sessionStorage.setItem("todoStorage", taskArray);
   displayTask(); //call the displayTask function
 }
 
 // delete all tasks
 function deleteAll(deleteAllCallback) {
   taskArray = []; //empty the array
-  localStorage.setItem("New Todo", JSON.stringify(taskArray)); //set the item in localstorage
+  sessionStorage.setItem("todoStorage", JSON.stringify(taskArray)); //set the item in sessionStorage
   displayTask(); //call the displayTask function
 }
